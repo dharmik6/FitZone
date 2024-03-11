1545c38799aebd58c7aaf22332db20a3240018f1
jaivi <jaivingadhiya12@gmail.com>
2024-03-11 12:28:40 +0530

    create Achievement 10/3/24

---
+++ app/src/main/java/com/example/fitzone/AchievementsList.java
@@ -0,0 +1,14 @@
+package com.example.fitzone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AchievementsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements_list);
+    }
+}
